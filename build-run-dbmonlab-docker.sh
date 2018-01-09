fe6516d6ec25d66edc16d667726096865b283e23
Thomas Klughardt <thomas.klughardt@osxlttklug.fritz.box>
2018-01-09 13:52:26 +0100

    done changes

--- build-run-dbmonlab-docker.sh
+++ build-run-dbmonlab-docker.sh
@@ -1,3 +1,11 @@
+# Builds the DB Mon Lab image and runs a container.
+
+# Deletes the DB Mon Lab containers and images and recreates them.
+# Consider getting rid of at least the last one, since it always pulls 8 GB. At least that one can stay without any issues.
+docker stop $(docker ps -qaf name=se-lab-dbmon)
+docker rm $(docker ps -qaf name=se-lab-dbmon)
+docker rmi $(docker images pistensau/dbmon_lab_instance -q)
+docker rmi $(docker images pistensau/dbmonlab -q)
 
 if [ ! -f controller.env ]; then
   echo "Can't find controller.env, exitting..."
@@ -14,6 +22,7 @@ echo ${CONTROLLER_HOST}
 echo "docker build -t pistensau/dbmon_lab_instance \
   --build-arg CONTROLLER_HOST=${CONTROLLER_HOST} \
   --build-arg CONTROLLER_PORT=${CONTROLLER_PORT} \
+  --build-arg SSL_ENABLED=${SSL_ENABLED} \
   --build-arg ACCOUNT_NAME=${ACCOUNT_NAME} \
   --build-arg ACCOUNT_ACCESS_KEY=${ACCOUNT_ACCESS_KEY} \
   --build-arg APPD_AGENT_VERSION=${APPD_AGENT_VERSION} ."
@@ -21,6 +30,7 @@ echo "docker build -t pistensau/dbmon_lab_instance \
 docker build -t pistensau/dbmon_lab_instance \
   --build-arg CONTROLLER_HOST=${CONTROLLER_HOST} \
   --build-arg CONTROLLER_PORT=${CONTROLLER_PORT} \
+  --build-arg SSL_ENABLED=${SSL_ENABLED} \
   --build-arg ACCOUNT_NAME=${ACCOUNT_NAME} \
   --build-arg ACCOUNT_ACCESS_KEY=${ACCOUNT_ACCESS_KEY} \
   --build-arg APPD_AGENT_VERSION=${APPD_AGENT_VERSION} .
